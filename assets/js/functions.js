c72c8fb039694bebd7a2eb26f8d80b41d07cf813
KRiPT1C <threalron.goyette@gmail.com>
2016-01-31 20:38:31 -0800

    update stuff

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+function initMap() {
+  var myLatLng = {lat: 21.0040225, lng: -156.6594173};
+  var mapDiv = document.getElementById('g-map');
+  var map = new google.maps.Map(mapDiv, {
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    scrollwheel: false,
+    zoom: 17
+  });
+  var image = 'assets/img/icons/g-map-pin.png';
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
